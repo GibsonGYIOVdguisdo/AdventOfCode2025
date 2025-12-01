6b62fe724ce3278fe0a6d3b4d4db41e8d387d822
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-12-01 10:39:42 +0000

    Created class for SafeDial with moveWheel method

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class SafeDial{
+  private:
+    int wheelPosition = 50;
+    int maxWheelPosition = 100;
+
+  public:
+    int moveWheel(int amount){
+      wheelPosition = (wheelPosition + amount) % maxWheelPosition;
+      if (wheelPosition < 0){
+        wheelPosition = maxWheelPosition - (wheelPosition * -1);
+      }
+      return wheelPosition;
+    }
+};
+
 int main()
 {
+  SafeDial safeDial;
+
+  cout << safeDial.moveWheel(-51);
+  
+  return 0;
 }
