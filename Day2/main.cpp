f280d61705a7c0d7a7c361ceb188b836622e31ca
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-12-03 18:34:00 +0000

    Solved puzzle 2
    Additionally fixed the sum of valid ids in range not using the parsed function for validity

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -14,13 +14,31 @@ bool isTwoRepeatingHalves(string value){
   return firstHalf == lastHalf;
 }
 
+bool isMadeOfRepeatingSubstring(string value){
+  for (int i = 0; i < value.length() / 2; i++){
+    string substring = value.substr(0, i + 1);
+    bool repeatingSubstringFound = true;
+    for (int j = i + 1; j < value.length(); j += i + 1){
+      if (value.substr(j, i + 1) != substring){
+        repeatingSubstringFound = false;
+        break;
+      }
+    }
+    if (repeatingSubstringFound){
+      return true;
+    }
+  }
+
+  return false;
+}
+
 long long getSumOfInvalidIdsInRange(long long start, long long end, function<bool (string)> isValid){
   long long sumOfInvalidIds = 0;
 
   for (long long currentId = start; currentId <= end; currentId++){
     string idString = to_string(currentId);
 
-    if (isTwoRepeatingHalves(idString)){
+    if (isValid(idString)){
       sumOfInvalidIds += currentId;
     }
   }
@@ -56,7 +74,28 @@ long long solvePuzzleOne(string input){
 }
 
 long long solvePuzzleTwo(string input){
-  return 0;
+  long long sumOfInvalidIds = 0;
+
+  while (input.length() != 0){
+    int endOfSegmentIndex = input.find(',');
+
+    string currentSegment;
+    if (endOfSegmentIndex == -1){
+      currentSegment = input;
+      input = "";
+    } else {
+      currentSegment = input.substr(0, endOfSegmentIndex);
+      input = input.substr(endOfSegmentIndex + 1);
+    }
+
+    int dividerPos = currentSegment.find('-');
+    long long start = stoll(currentSegment.substr(0, dividerPos));
+    long long end = stoll(currentSegment.substr(dividerPos + 1));
+
+    sumOfInvalidIds += getSumOfInvalidIdsInRange(start, end, isMadeOfRepeatingSubstring);
+  }
+
+  return sumOfInvalidIds;
 }
 
 int main()
@@ -66,7 +105,7 @@ int main()
   inputStream >> idRanges;
   inputStream.close();
 
-  cout << solvePuzzleOne(idRanges);
+  cout << "2: " << solvePuzzleTwo(idRanges) << endl;
 
   return 0;
 }
