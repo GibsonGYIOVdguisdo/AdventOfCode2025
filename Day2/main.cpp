59b33b15c21307ef4b63db42be06b7e01b00aa56
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-12-03 17:52:40 +0000

    Moved code getting invalid inputs in range to its own function

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -3,6 +3,46 @@
 
 using namespace std;
 
+long long getSumOfInvalidIdsInRange(long long start, long long end){
+  long long sumOfInvalidIds = 0;
+
+  for (long long currentId = start; currentId <= end; currentId++){
+    string idString = to_string(currentId);
+    int midPoint = idString.length() / 2;
+    if (idString.substr(0, midPoint) == idString.substr(midPoint)){
+      sumOfInvalidIds += currentId;
+    }
+  }
+
+  return sumOfInvalidIds;
+}
+
+long long solvePuzzleOne(){
+  string input;
+  cin >> input;
+  long long sumOfInvalidIds = 0;
+
+  while (input.length() != 0){
+    int endOfSegmentIndex = input.find(',');
+
+    string currentSegment;
+    if (endOfSegmentIndex == -1){
+      currentSegment = input;
+      input = "";
+    } else {
+      currentSegment = input.substr(0, endOfSegmentIndex);
+      input = input.substr(endOfSegmentIndex + 1);
+    }
+
+    int dividerPos = currentSegment.find('-');
+    long long start = stoll(currentSegment.substr(0, dividerPos));
+    long long end = stoll(currentSegment.substr(dividerPos + 1));
+
+    sumOfInvalidIds += getSumOfInvalidIdsInRange(start, end);
+  }
+
+  cout << "total: " << sumOfInvalidIds;
+}
 
 int main()
 {
