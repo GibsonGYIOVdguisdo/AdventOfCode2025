ff80120c38a718d2c50b379e276352fbce83d3c9
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-12-05 18:19:32 +0000

    Moved puzzle 1 into its own logic

--- Day5/main.py
+++ Day5/main.py
@@ -4,26 +4,28 @@ f = open("input.txt", "r")
 file_content = f.readlines()
 f.close()
 
-  
-line_number = 0
-fresh_id_ranges = []
+def solve_puzzle_one(file_content):
+  line_number = 0
+  fresh_id_ranges = []
 
-for i, line in enumerate(file_content):
-  if (len(line.strip()) == 0):
-    line_number = i + 1
-    break
-  
-  start, end = line.strip().split("-")
-  start = int(start)
-  end = int(end)
-  
-  fresh_id_ranges.append([start, end])
-  
-total = 0
-for id in file_content[line_number:]:
-  for range in fresh_id_ranges:
-    if int(id) > range[0] and int(id) <= range[1]:
-      total += 1
+  for i, line in enumerate(file_content):
+    if (len(line.strip()) == 0):
+      line_number = i + 1
       break
+    
+    start, end = line.strip().split("-")
+    start = int(start)
+    end = int(end)
+    
+    fresh_id_ranges.append([start, end])
+    
+  total = 0
+  for id in file_content[line_number:]:
+    for range in fresh_id_ranges:
+      if int(id) > range[0] and int(id) <= range[1]:
+        total += 1
+        break
+  
+  return total
 
-print (total)
+print (solve_puzzle_one(file_content))
